3a01e4ee63434a698f6fd2adb7ed543b0c60ad84
ZeynepDuzen <zeynepduzen59@gmail.com>
2023-07-16 15:30:28 +0300

    Proje dosyası ekle.

---
+++ convertDouble2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace convertDouble2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //klavyeden girilen ondalıklı iki sayiyi toplayan kod blogu
+            double sayi1, sayi2, toplam;
+            Console.WriteLine("Lütfen toplamak istediğiniz iki ondalıklı sayidan ilkini girin : ");
+            Console.WriteLine();
+            sayi1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            Console.WriteLine("lütfen ikinci sayiyi giriniz : ");
+            Console.WriteLine();
+            sayi2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine();
+            toplam = sayi1 + sayi2;
+            Console.WriteLine("Girmis oldugunuz ondalıklı sayilarin toplami : " + toplam);
+
+            Console.Read();
+        }
+    }
+}
